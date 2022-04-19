e8313eefc96a667e488b04182cbabc1bf36f2161
Gerlad Hurtado <raudalesgo@gmail.com>
2022-04-18 22:26:55 -0600

    add provability to rain

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,27 +3,29 @@ const request = require("request");
 const forecast = (lat, log, callback) => {
   const url =
     "http://api.weatherstack.com/current?access_key=7b2fa481aefe9630bc9b80feaa2ecf0a&query=" +
-    lat+
+    lat +
     "," +
     log +
     "&units=s";
 
-    request ( {uri: url, json:true},(error, response, body) => {
-            if (error) {
-                callback("Unable to connect to location services!", undefined);
-            } else if (body.length === 0) {
-                callback("Unable to find location try another search!", undefined);
-            } else {
-                callback(undefined,
-                    body.current.weather_descriptions +
-                    " the currently temperature is " +
-                    body.current.temperature +
-                    " its feels like " +
-                    body.current.feelslike
-                );
-            }
-        })
-
+  request({ uri: url, json: true }, (error, response, body) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.length === 0) {
+      callback("Unable to find location try another search!", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions +
+          " the currently temperature is " +
+          body.current.temperature +
+          " its feels like " +
+          body.current.feelslike +
+          " Precipitation Probablity is " +
+          body.current.precip
+      );
+    }
+  });
 };
 
 module.exports = forecast;
